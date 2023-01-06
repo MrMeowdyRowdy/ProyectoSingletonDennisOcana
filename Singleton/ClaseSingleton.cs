8bbf57c1f1c9c56d29507a0e2a7f55caf0153bc4
Dennis Steven Ocaña Santamaria <dennis.ocana@udla.edu.ec>
2023-01-05 22:41:43 -0500

    Agregar archivos de proyecto.

---
+++ Singleton/ClaseSingleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class ClaseSingleton
+    {
+        private static ClaseSingleton instance = null;
+        public string mensajeSingleton;
+
+        protected ClaseSingleton()
+        {
+            mensajeSingleton = "Saludos de vuelta mundo exterior, son un Singleton";
+        }
+
+        public static ClaseSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ClaseSingleton();
+
+                return instance;
+            }
+        }
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] arga)
+        {
+            Console.WriteLine(ClaseSingleton.Instance.mensaje);
+            ClaseSingleton.Instance.mensaje = "Hola clase Singleton";
+            Console.WriteLine(ClaseSingleton.Instance.mensaje);
+        }
+    }
+}
